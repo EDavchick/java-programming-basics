b68e06a2ee4eea318e165f85770aef1ecbdb6cc5
ED <154407386+EDavchick@users.noreply.github.com>
2025-07-31 13:11:58 +0400

    done lesson1 level6 task2

---
+++ src/main/java/org/javaguru/elena_davydova/lesson_01/MultiplicationTwoNumbers.java
@@ -0,0 +1,18 @@
+package org.javaguru.elena_davydova.lesson_01;
+
+/**
+ * Напишите Java программу, которая выведет на консоль результат умножения двух чисел.
+ * Числа можно придумать любые, их не надо запрашивать у пользователя.
+ * Тестовые данные:
+ * 50 * 3
+ * Ожидаемый результат:
+ * 150
+ */
+public class MultiplicationTwoNumbers {
+    public static void main(String[] args) {
+        int firstNum = 50;
+        int secondNum = 3;
+
+        System.out.println(firstNum * secondNum);
+    }
+}
