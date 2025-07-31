f9d333614bf7e86da957023e07df386247d78a38
ED <154407386+EDavchick@users.noreply.github.com>
2025-07-31 13:09:12 +0400

    done lesson1 level6 task1

---
+++ src/main/java/org/javaguru/elena_davydova/lesson_01/DivideTwoNumbers.java
@@ -0,0 +1,17 @@
+package org.javaguru.elena_davydova.lesson_01;
+
+/**
+ * Write a Java program to divide two numbers and print on the screen.
+ * Test Data :
+ * 50/3
+ * Expected Output :
+ * 16
+ */
+public class DivideTwoNumbers {
+    public static void main(String[] args) {
+        int firstNum = 50;
+        int secondNum = 3;
+
+        System.out.println(firstNum / secondNum);
+    }
+}
---
+++ src/main/java/org/javaguru/elena_davydova/lesson_01/SumTwoNumbers.java
@@ -0,0 +1,21 @@
+package org.javaguru.elena_davydova.lesson_01;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Напишите Java программу, которая выведет на консоль сумму двух чисел.
+ * Числа можно придумать любые, их не надо запрашивать у пользователя.
+ * Тестовые данные:
+ * 74 + 36
+ * Ожидаемый результат:
+ * 110
+ */
+public class SumTwoNumbers {
+    public static void main(String[] args) {
+        int firstNum = 74;
+        int secondNum = 36;
+
+        System.out.println(firstNum + secondNum);
+    }
+}
