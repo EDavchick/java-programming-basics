398889c4af28e91a8ffa5a4faa4a8929d3aef321
ED <154407386+EDavchick@users.noreply.github.com>
2025-08-03 19:20:14 +0400

    done lesson2 level4

---
+++ src/main/java/org/javaguru/elena_davydova/lesson_02/level_4/PrintCommands.java
@@ -0,0 +1,11 @@
+package org.javaguru.elena_davydova.lesson_02.level_4;
+
+public class PrintCommands {
+    public static void main(String[] args) {
+        System.out.print("text");
+        System.out.println("text");
+        System.out.print("text");
+        // texttext
+        // text
+    }
+}
