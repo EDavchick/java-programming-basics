634ea45748f423676532da644b3bacaededa324d
ED <154407386+EDavchick@users.noreply.github.com>
2025-08-03 19:35:50 +0400

    done lesson2 level6-7

---
+++ src/main/java/org/javaguru/elena_davydova/lesson_02/level_7/NoSleepNoFocus.java
@@ -0,0 +1,8 @@
+package org.javaguru.elena_davydova.lesson_02.level_7;
+
+public class NoSleepNoFocus {
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+}
