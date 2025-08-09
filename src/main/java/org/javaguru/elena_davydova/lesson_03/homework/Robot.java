0f685570d4585480afad58066ffa7638c49a06fc
ED <154407386+EDavchick@users.noreply.github.com>
2025-08-09 18:59:36 +0400

    done lesson3 hw level1

---
+++ src/main/java/org/javaguru/elena_davydova/lesson_03/homework/Robot.java
@@ -0,0 +1,17 @@
+package org.javaguru.elena_davydova.lesson_03.homework;
+
+public class Robot {
+    String robotName;
+
+    public Robot(String robotName) {
+        this.robotName = robotName;
+    }
+
+    public String getRobotName() {
+        return robotName;
+    }
+
+    public void setRobotName(String robotName) {
+        this.robotName = robotName;
+    }
+}
---
+++ src/main/java/org/javaguru/elena_davydova/lesson_03/homework/RobotApp.java
@@ -0,0 +1,16 @@
+package org.javaguru.elena_davydova.lesson_03.homework;
+
+public class RobotApp {
+    public static void main(String[] args) {
+        Robot robot1 = new Robot("Sam");
+        Robot robot2 = new Robot("Petya");
+
+        System.out.println("Name robot1 is " + robot1.getRobotName());
+        System.out.println("Name robot2 is " + robot2.getRobotName());
+
+        robot1.setRobotName("Root");
+        robot2.setRobotName("Kolya");
+        System.out.println("Now name robot1 is " + robot1.getRobotName());
+        System.out.println("Now name robot2 is " + robot2.getRobotName());
+    }
+}
